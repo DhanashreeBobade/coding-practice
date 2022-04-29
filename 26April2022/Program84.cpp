df407b70a48d067dbb7f9249e4047cb78db643da
dhanashree.bobade <dhanashreebobade05@gmail.com>
2022-04-30 01:51:11 +0530

    Code updated:

--- 26April2022/Program84.cpp
+++ 26April2022/Program84.cpp
@@ -1,13 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main( )
+int main()
 {
-    int i,n, a[100],m;
+    int i, count = 0, n, temp, a[500], j;
+    cout << "Write a Program to Convert entered Decimal Number to Octal Number" << endl;
+    cout << "Enter a positive decimal number : ";
+    cin >> n;
+    temp = n;
 
-    cout<<"Write a Program to Convert entered Decimal Number to Octal Number"<<endl;
-    cout<<"Enter a Decimal Number : ";
-    cin>>n;
-m=
-    
+        while (n != 0)
+        {
+            n = n / 8;
+            ++count;
+        }
+        // cout << count << endl;
+        n = temp;
+
+        for (i = 0; i < count; i++)
+        {
+            j = n % 8;
+            n = n / 8;
+            a[i] = j;
+        }
+
+        for (i = 0; i < count; i++)
+        {
+            cout << a[count - i - 1];
+        }
+   
     return 0;
 }
